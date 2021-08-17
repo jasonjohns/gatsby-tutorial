05b70f7fadd0f56b40aa66d3e58d08f35bb2bc9e
Jason Johns <jason@jasonjohns.com>
2021-08-17 15:41:48 -0400

    finished gatsby tutorial part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../components/layout'
 
@@ -5,6 +6,9 @@ const AboutPage = () => {
     return(
         <Layout pageTitle="About Me!">
             <p>Hi there! I'm the proud creaor of this site, which I built with Gatsby!</p>
+            <StaticImage alt="Memetastic"
+                         src="../images/huga9h36j6u61.jpeg"
+            />
         </Layout>
     )
 }
